6c622c94e2b044a22c18c7da4f3708263fab587c
jaqque <jaqque@gmail.com>
2021-03-15 16:56:07 -0700

    Added some stuff.
    Read the diff yourself ya lazy bastard
    
    Have the g back: g

--- thingy.zsh
+++ thingy.zsh
@@ -11,6 +11,8 @@ local cha=0
 local lck=0
 local bonus_points=0
 
+local menu=($abilities level bonus help quit)
+
 local skills_active=''
 local skills_passive='' # wtf are these?
 
@@ -19,19 +21,25 @@ local total_points=0
 local spent_points=0
 local available_points=0
 
+help() {
+  echo "HA! As if."
+}
+
 read "name?Name? "
 read "level?Level? "
 read "bonus_points?BONUS? as if you deserve one "
-if false; then
+echo "Original, level one scores:"
+if true; then
 for ability in $abilities; do
-  read "$ability?$ability "
+  read "$ability?$ability: "
 done
 fi
+read "skills_active?Fucking active skills: "
+read "skills_passive?Fucking passive skills: "
 
-str=45
 dump_state () {
   for var in name level $abilities; do
-    echo "$var: ${(P)var}"
+    echo "-> $var: ${(P)var}"
   done
 }
 
@@ -41,32 +49,55 @@ points_from_level() {
 }
 total_points=$(points_from_level $level)
 : $(( total_points+=bonus_points ))
-dump_state; echo "POINTS!!!!! $total_points"
+echo "POINTS!!!!! $total_points"
 
 level_cost() {
   current_level=$1
   echo $(( $current_level/10 + 1))
 }
-loop=true
 
-echo "$abilities"
+loop=true
 while $loop; do
+  echo "==> MENU: $menu"
   available_points=$(( total_points - spent_points ))
   dump_state
   echo "You have $available_points to spend. Spend wisely"
-  read "ability?What to upgrade? (quit to quit) "
+  read "ability?Select: "
   case $ability in
-    (q|quit|e|exit) loop=false ; continue ;;
+    (q|Q|quit|e|Q|exit) loop=false ; continue ;;
+    help) help ;;
+    level)
+      echo "Level up!";
+      : $(( level++ )) ;
+      total_points=$(( $(points_from_level $level) + bonus_points ));
+      ;;
+    bonus)
+      echo "You have $bonus_points currently."
+      read "bonus_points?New value: "
+      total_points=$(( $(points_from_level $level) + bonus_points ));
+      ;;
   esac
   (( ${abilities[(I)$ability]} )) || continue
-  cost=$(level_cost ${(P)ability})
-  print "That will cost you $cost points."
+  read "direction?Up or Down? "
+  case $direction in
+    d*|D*) cost=$(level_cost $(( ${(P)ability} - 1 )) ); net=earn;;
+    u*|U*) cost=$(level_cost ${(P)ability}); net=cost ;;
+  esac
+  print "That will $net you $cost points."
   read "verify?Are you sure? "
   case $verify in
-    y|Y|yes|Yes|YES|sure|whatever) ;;
+    y*|Y*|sure|whatever) ;;
     *) echo "Your loss."; continue ;;
   esac
-  (( cost > available_points )) && { print "Nice try, dipshit"; continue ; }
-  : $(( $ability++ ))
-  : $(( spent_points+=cost ))
+  case $net in
+    cost)
+      (( cost > available_points )) && { print "Nice try, dipshit"; continue ; }
+      : $(( $ability++ ))
+      : $(( spent_points+=cost ))
+      ;;
+    earn)
+      : $(( $ability-- ))
+      : $(( spent_points-=cost ))
+      ;;
+  esac
 done
