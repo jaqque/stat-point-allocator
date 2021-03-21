8a6a2fa35f5bda80eae18957bdd3bea75e0c83d6
jaqque <jaqque@gmail.com>
2021-03-21 02:49:30 +0000

    Make Level Up button work!
    Well, "work".
    
    It doesn't really do a lot... but it does indeed work.

--- thingy.py
+++ thingy.py
@@ -1,32 +1,39 @@
 from flask import Flask
-from flask import render_template_string, render_template
+from flask import render_template_string, render_template, request
 
 app = Flask(__name__)
+
+
+name="William Frederickson, Esq."
+level=0
+abilities = {
+    "STR": [0,"Strength"],
+    "END": [0,"Endurance"],
+    "AGI": [0,"Agility"],
+    "INT": [0,"Intelligence"],
+    "CHA": [0,"Charisma"],
+    "LCK": [0,"Luck"],
+}
+bonus_points=0
+skills = {
+    "passive": ["","Passive"],
+    "active": ["","Active"],
+}
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/try')
+@app.route('/try', methods=['POST', 'GET'])
 def tired():
-    name="Fred"
-    level=0
-    abilities = {
-        "STR": [0,"Strength"],
-        "END": [0,"Endurance"],
-        "AGI": [0,"Agility"],
-        "INT": [0,"Intelligence"],
-        "CHA": [0,"Charisma"],
-        "LCK": [0,"Luck"],
-    }
-    bonus_points=0
-    skills = {
-        "passive": ["","Passive"],
-        "active": ["","Active"],
-    }
+    global level
 
-    page=render_template("stat-template.html.j2",title="GET TO THE SPA-TER!!!", abilities=abilities, skills=skills)
+    if request.method == 'POST':
+        level+=1
+
+    page=render_template("stat-template.html.j2",title="GET TO THE SPA-TER!!!", abilities=abilities, skills=skills, uri=request.base_url, level=level, name=name)
     return page
 
 if __name__ == '__main__':
