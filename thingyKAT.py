3362f84945aebb4f7e6157f1757f79fac6328617
kinadra <kat@sbih.org>
2021-07-05 14:18:39 -0700

    Add files via upload

---
+++ thingyKAT.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import render_template_string, render_template, request
+
+app = Flask(__name__)
+
+## GLOBALS!! we like globals.
+name = request.form['name']
+level=0
+abilities = {
+    "STR": [0,"Strength"],
+    "END": [0,"Endurance"],
+    "AGI": [0,"Agility"],
+    "INT": [0,"Intelligence"],
+    "CHA": [0,"Charisma"],
+    "LCK": [0,"Luck"],
+}
+bonus_points=0
+skills = {
+    "passive": ["","Passive"],
+    "active": ["","Active"],
+}
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/try', methods=['POST', 'GET'])
+def tired():
+    global level
+
+    if request.method == 'POST':
+        if request.form['submit']=='levelup':
+            level+=1
+        for ability in abilities:
+            if request.form['submit']==f"{ability}up":
+              abilities[ability][0]+=1
+            if request.form['submit']==f"{ability}down":
+              abilities[ability][0]-=1
+
+points_remaining = (points - points_spent)
+point_cost = (ability / 10 + 1)
+points = ((level*level + level )/2 + level - 2)
+
+strcurrent = strinit + stradd
+endcurrent = endinit + endadd
+agicurrent = agiinit + agiadd
+intcurrent = intinit + intadd
+chacurrent = chainit + chaadd
+lckcurrent = lckinit + lckadd
+
+    page=render_template("stat-template.html.j2",title="GET TO THE SPA-TER!!!", abilities=abilities, skills=skills, uri=request.base_url, level=level, name=name)
+    return page
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
